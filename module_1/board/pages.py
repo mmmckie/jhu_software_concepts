c6e2c08eee975c9eddf157e51d590d0582a90af1
mmmckie <maxmckie@gmail.com>
2026-01-22 19:40:35 -0600

    Edited bio and project description content, added navbar highlighting.

--- module_1/board/pages.py
+++ module_1/board/pages.py
@@ -4,12 +4,12 @@ bp = Blueprint("pages", __name__)
 
 @bp.route("/")
 def home():
-    return render_template("pages/home.html")
+    return render_template("pages/home.html", active = 'home')
 
 @bp.route("/contact")
 def contact():
-    return render_template("pages/contact.html")
+    return render_template("pages/contact.html", active = 'contact')
 
 @bp.route("/projects")
 def projects():
-    return render_template("pages/projects.html")
+    return render_template("pages/projects.html", active = 'projects')
