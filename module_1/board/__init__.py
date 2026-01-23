5ff81b209eab342b55b9773a6f79806af5d4402e
mmmckie <maxmckie@gmail.com>
2026-01-22 19:20:31 -0600

    Added contact icons and formatting, added css formatting for list elements on publications

--- module_1/board/__init__.py
+++ module_1/board/__init__.py
@@ -1,15 +1,3 @@
-
-# from flask import Flask
-
-# def create_app():
-#     app = Flask(__name__)
-
-#     from app.blueprints.main import main_bp
-#     app.register_blueprint(main_bp)
-
-#     return app
-
-
 from flask import Flask
 
 from board import pages
