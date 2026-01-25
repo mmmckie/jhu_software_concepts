d803ddb941cc0f5a7cc10ac63198af8b899369ee
Max <maxmckie@gmail.com>
2026-01-25 17:09:48 -0600

    Initial module 2 commit

---
+++ module_2/clean.py
@@ -0,0 +1,23 @@
+import requests
+import urllib3
+
+from bs4 import BeautifulSoup
+import re
+
+import json
+
+
+def clean_data():
+    "Converts data into a structured format."
+    pass
+
+
+def save_data():
+    "Saves cleaned data into applicant_data.json."
+    pass
+
+
+def load_data():
+    "Loads cleaned data from applicant_data.json."
+    pass
+
---
+++ module_2/scrape.py
@@ -0,0 +1,11 @@
+import requests
+import urllib3
+
+from bs4 import BeautifulSoup
+import re
+
+import json
+
+def scrape_data():
+    "Pulls admissions data from GradCafe."
+    pass
